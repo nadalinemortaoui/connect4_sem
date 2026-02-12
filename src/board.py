047d8f8e80140f4a1188f7c2c75c0ac69ed7efd7
nadalinemortaoui <s56666@edu.campus02.at>
2026-02-12 06:21:40 +0100

    "Add missing colon and resolve import errors"

--- src/board.py
+++ src/board.py
@@ -1,12 +1,5 @@
 class Board:
-    """
-    Represents the Connect Four game board.
-    """
-
     def __init__(self):
-        """
-        Initializes an empty 6x7 game board.
-        """
         self.grid = [ #Game Board Structure, 0 = empty field
         [0, 0, 0, 0, 0, 0, 0], #First Row from Above
         [0, 0, 0, 0, 0, 0, 0],
@@ -18,9 +11,6 @@ class Board:
 
 
     def is_valid_move(self, column):
-        """
-        Checks whether a move in the given column is valid.
-        """
         if column < 0 or column >= 7:
             return False
 
@@ -31,19 +21,13 @@ class Board:
 
 
     def drop_stone(self, column, player):
-        """
-        Drops a stone into the given column for the given player.
-        """
-        for row in range(5, -1, -1) #Checks if field is empty
+        for row in range(5, -1, -1): #Checks if field is empty
             if self.grid[row][column] == 0:
                 self.grid[row][column] = player
                 return
 
 
     def check_win(self, player):
-        """
-        Checks whether the given player has won the game.
-        """
         for row in range(6):
             for col in range(4):
                 if (
@@ -54,7 +38,7 @@ class Board:
                 ):
                     return True
 
-            # Vertical 
+            #Vertical
         for row in range(3):
             for col in range(7):
                 if (
@@ -84,7 +68,7 @@ class Board:
                         self.grid[row + 1][col - 1] == player and
                         self.grid[row + 2][col - 2] == player and
                         self.grid[row + 3][col - 3] == player
-                ):
+                    ):
                     return True
 
         return False
